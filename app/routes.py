59d2b111afe63912451780026101a9b124e70e71
Ruge Li <rugeli0605@gmail.com>
2022-04-25 23:25:09 -0700

    refectored by using helper function and abort

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify,abort,make_response
 
 class Book:
     def __init__(self, id, title, description):
@@ -14,6 +14,19 @@ books = [
 ]
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
+
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        abort(make_response({"message":f"book {book_id} invalid"}, 400))
+
+    for book in books:
+        if book.id == book_id:
+            return book
+
+    abort(make_response({"message":f"book {book_id} not found"}, 404))
 
 @books_bp.route("", methods=["GET"])
 def handle_books():
@@ -30,16 +43,10 @@ def handle_books():
 
 @books_bp.route("/<book_id>", methods=["GET"])
 def handle_book(book_id):
-    try:
-        book_id = int(book_id)
-    except:
-        return {"message":f"book {book_id} invalid"}, 400
-    for book in books:
-        if book.id == book_id:
-            return {
+    book = validate_book(book_id)
+
+    return {
                 "id": book.id,
                 "title": book.title,
                 "description": book.description,
-            }
-
-    return {"message":f"book {book_id} not found"}, 404
+    }
