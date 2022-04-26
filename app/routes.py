b22b24418fb15faae8d611d1e4797bb12b8590cb
Ruge Li <rugeli0605@gmail.com>
2022-04-25 22:16:19 -0700

    read all books

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,32 @@
 
-from flask import Blueprint
+from flask import Blueprint,jsonify
+
+class Book:
+    def __init__(self, id, title, description):
+        self.id = id
+        self.title = title
+        self.description = description
+
+books = [
+    Book(1, "Fictional Book Title A", "A fantasy novel set in an imaginary world."),
+    Book(2, "Fictional Book Title B", "A fantasy novel set in an imaginary world."),
+    Book(3, "Fictional Book Title C", "A fantasy novel set in an imaginary world.")
+]
 
 hello_world_bp = Blueprint("hello_world_bp", __name__)
+books_bp =Blueprint("books",__name__,url_prefix="/books")
+
+@books_bp.route("", methods=["GET"])
+def handle_books():
+    books_response = []
+    for book in books:
+        books_response.append({
+            "id": book.id,
+            "title": book.title,
+            "description": book.description
+        })
+    return jsonify(books_response)
+
 
 
 @hello_world_bp.route("/hello-world", methods=["GET"])
