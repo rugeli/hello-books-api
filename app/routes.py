24df8a42d8fead09cad9976fa3f43782f8d6157f
Ruge Li <rugeli0605@gmail.com>
2022-04-28 23:45:36 -0700

    handled POST and GET

--- app/routes.py
+++ app/routes.py
@@ -18,19 +18,33 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-@books_bp.route("", methods=["POST"])
+@books_bp.route("", methods=["POST","GET"])
 def handle_books():
-    request_body = request.get_json()
-    if "title" not in request_body or "description" not in request_body:
-        return make_response("Invalid Request", 400)
+    if request.method == "POST":
+        request_body = request.get_json()
+        if "title" not in request_body or "description" not in request_body:
+            return make_response("Invalid Request", 400)
 
-    new_book = Book(title=request_body["title"],
-                    description=request_body["description"])
+        new_book = Book(title=request_body["title"],
+                        description=request_body["description"])
 
-    db.session.add(new_book)
-    db.session.commit()
+        db.session.add(new_book)
+        db.session.commit()
 
-    return make_response(f"Book {new_book.title} successfully created", 201)
+        return make_response(f"Book {new_book.title} successfully created", 201)
+    
+    elif request.method == "GET":
+        books = Book.query.all()
+        books_response = []
+        for book in books:
+            books_response.append(
+                {
+                    "id": book.id,
+                    "title": book.title,
+                    "description": book.description
+                }
+            )
+        return jsonify(books_response)
 
 #def validate_book(book_id):
 #    try:
