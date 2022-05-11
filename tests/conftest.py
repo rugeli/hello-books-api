5a59599485c5f29dfa6a873c920e7b14c8c3320c
Ruge Li <rugeli0605@gmail.com>
2022-05-10 18:01:21 -0700

    add Procfile

--- tests/conftest.py
+++ tests/conftest.py
@@ -28,10 +28,12 @@ def client(app):
 @pytest.fixture
 def two_saved_books(app):
     # Arrange
-    ocean_book = Book(title="Ocean Book",
-                      description="watr 4evr")
-    mountain_book = Book(title="Mountain Book",
-                         description="i luv 2 climb rocks")
+    ocean_book = Book(
+        title="Ocean Book",
+        description="watr 4evr")
+    mountain_book = Book(
+        title="Mountain Book",
+        description="i luv 2 climb rocks")
 
     db.session.add_all([ocean_book, mountain_book])
     # Alternatively, we could do
